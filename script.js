a89b7697e2bd49e58f872ace3d589a3d5b6f83ca
rmnk-nz <rua.manukau20@gmail.com>
2021-09-21 11:11:23 +1000

    added function to save user input to local data

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 //Display current day and date
 var today = moment();
-$("#currentDay").text(today.format("dddd, MMM Do YYYY"));
+$("#currentDay").text(today.format("ddd, MMM Do YYYY"));
+
+
+//click save button to save user input to the local data
+$(".saveBtn").on("click", function () { 
+    confirm("Save to Schedule?");
+    var userInput = $(this).siblings(".description").val();
+    var userHour =  $(this).parent().attr("id");
+
+    localStorage.setItem(userHour, userInput);
+});
