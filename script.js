4b68dfc0926538af4d0831a80ab26849ed325915
rmnk-nz <rua.manukau20@gmail.com>
2021-09-22 12:09:35 +1000

    changed for loop to each function to follow current hour

--- script.js
+++ script.js
@@ -19,11 +19,10 @@ function followHour() {
     //follows current hour by moment.js libary
     var currentHour = moment().hour();
 
-    // selects each time block and gets the id hour associated with that time block 
-    var listElement = $(".time-block");
-        for (var i = 0; i < listElement.lenght; i++) {
-            var timeBlock = parseInt($(listElement[i]).attr("id").split("hour")[0]);
-        }
+    // selects each time block and gets the id hour associated with that time block
+    $(".time-block").each(function () {
+        var timeBlock = parseInt($(this).attr("id"));
+  
         //apply css when the hour is behind current hour
         if (timeBlock < currentHour) {
             $(this).addClass("past");
@@ -42,23 +41,18 @@ function followHour() {
             $(this).removeClass("present");
             $(this).addClass("future");
         }
-}
+    })
+} followHour()
 
 
-    //Retrieve data from local storage
-    $("#8hour .description").val(localStorage.getItem("8hour"));
-    $("#9hour .description").val(localStorage.getItem("9hour"));
-    $("#10hour .description").val(localStorage.getItem("10hour"));
-    $("#11hour .description").val(localStorage.getItem("11hour"));
-    $("#12hour .description").val(localStorage.getItem("12hour"));
-    $("#13hour .description").val(localStorage.getItem("13hour"));
-    $("#14hour .description").val(localStorage.getItem("14hour"));
-    $("#15hour .description").val(localStorage.getItem("15hour"));
-    $("#16hour .description").val(localStorage.getItem("16hour"));
-    $("#17hour .description").val(localStorage.getItem("17hour"));
-
-
-// var listElement = $(".time-block");
-// for(var i=0; i< listElement.lenght; i++){
-//     var blockTime = parseInt($(listElement[i]).attr("id").split("hour")[1]);
-// }
+//Retrieve data from local storage
+$("#8hour .description").val(localStorage.getItem("8hour"));
+$("#9hour .description").val(localStorage.getItem("9hour"));
+$("#10hour .description").val(localStorage.getItem("10hour"));
+$("#11hour .description").val(localStorage.getItem("11hour"));
+$("#12hour .description").val(localStorage.getItem("12hour"));
+$("#13hour .description").val(localStorage.getItem("13hour"));
+$("#14hour .description").val(localStorage.getItem("14hour"));
+$("#15hour .description").val(localStorage.getItem("15hour"));
+$("#16hour .description").val(localStorage.getItem("16hour"));
+$("#17hour .description").val(localStorage.getItem("17hour"));
